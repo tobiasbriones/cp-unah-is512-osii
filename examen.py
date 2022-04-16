1a572ecd320c9915a765b00f2245995e272cda06
Tobias Briones <39544153+tobiasbriones@users.noreply.github.com>
2022-04-15 22:43:53 -0600

    Add files via upload
    This was the first exam I presented to the class.

---
+++ examen.py
@@ -0,0 +1,88 @@
+from Tkinter import *
+import os
+import subprocess
+import tkFont
+import tkMessageBox
+import ScrolledText
+import time
+# Tobias Briones
+
+# Link del video: https://youtu.be/d11WSVjmeeI
+
+def portlist():
+    os.system("sudo ufw status > portlist.txt")
+    return subprocess.check_output("cat /home/ubuntu1/portlist.txt", shell=True)
+
+class MainWindow:
+    def __init__(self):
+        self.mw = Tk()
+        self.update_btn = Button(self.mw)
+        self.allow_btn = Button(self.mw)
+        self.deny_btn = Button(self.mw)
+        self.textarea = ScrolledText.ScrolledText(self.mw)
+        self.allow_tf = Entry(self.mw)
+        self.deny_tf = Entry(self.mw)
+
+    def init(self):
+        self.mw.title("Firewall Linux")
+        self.mw.geometry("500x420")
+
+        self.update_btn.place(x=300, y=10)
+        self.update_btn.config(
+            text="Actualizar lista de puertos",
+            command=self.__on_update
+        )
+        self.textarea.config(
+            width=60,
+            height=20,
+            state=DISABLED
+        )
+        self.textarea.place(x=10, y=60)
+        
+        self.allow_tf.width=10
+        self.allow_tf.place(x=10, y=10)
+        self.allow_btn.config(
+            text="Allow",
+            command=self.__on_allow
+        )
+        self.allow_btn.place(
+            x=200,
+            y=10
+        )
+        
+        self.deny_tf.width=5
+        self.deny_tf.place(x=10, y=360)
+        self.deny_btn.config(
+            text="Deny",
+            command=self.__on_deny
+        )
+        self.deny_btn.place(
+            x=200,
+            y=360
+        )
+
+        self.mw.mainloop()
+
+    def __on_update(self):
+        value = portlist()
+
+        self.textarea.config(state=NORMAL)
+        self.textarea.delete("1.0", "end")
+        self.textarea.insert(INSERT, value)
+        self.textarea.config(state=DISABLED)
+        
+    def __on_allow(self):
+        port_value = self.allow_tf.get()
+        cmd1 = "sudo ufw allow "
+        cmd = (str(cmd1) + str(port_value))
+        os.system(cmd)
+        
+    def __on_deny(self):
+        port_value = self.deny_tf.get()
+        cmd1 = "sudo ufw deny "
+        cmd = (str(cmd1) + str(port_value))
+        os.system(cmd)
+
+mw = MainWindow()
+
+mw.init()
